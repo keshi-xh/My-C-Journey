04a10f37f19a5133c9b545c4977548d8854b364f
keshix <keshangeethanjana38@gmail.com>
2025-02-11 14:25:42 +0530

    added data types and variables

--- README.md
+++ README.md
@@ -3,7 +3,9 @@ i have no clue ok
 so i will be like updating this everyday if possible
 let's see how far i will go 
 so i found this site https://www.learn-c.org 
-and i will be folloing it ok 
+and i will be following it ok 
 first i thought it will be easy using chat gpt
 but then i thought to find a book(PDF) somewhere
 couldn't find then found the site
+so how i do this is i read and do the exercise in that site and then kinda copy and paste the code back to vs code to create the file and then update it at the end of the day
+it's pretty nice having chatgpt it helps a lot  
---
+++ data_types_and_variables.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+    int a = 3;
+    float b = 4.5;
+    double c = 5.25;
+    float sum;
+
+  /* Your code goes here */
+    sum = a+b+c; 
+    printf("The sum of a, b, and c is %f.", sum);
+    return 0;
+}
+/*so i did learn data types in here from the site ok*/
+/*so integers are defined using char, int, long, long long and short*/
+/*then there is unsigned integers i hope it means what means so basically if we define 'a' as an int */
+/*i think that's how it works then there are floats defined using float and double */
+/*what does double me, no clue*/
+/*#define BOOL char
+#define FALSE 0
+#define TRUE 1
+for this also i have no much clue*/
